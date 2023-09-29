a9b163fed049b9315649c8174834e811473de1e5
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-09-29 16:13:10 +0600

    Update js file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
 import './index.css';
 // main.js
-import { getLeaderboard, addEntry } from './leaderboard.js';
 
-// Function to display leaderboard in the HTML
-function displayLeaderboard() {
-  const leaderboard = document.getElementById('leaderboard');
-  leaderboard.innerHTML = '';
-
-  const leaderboardData = getLeaderboard();
-  leaderboardData.forEach((entry) => {
-    const entryElement = document.createElement('div');
-    entryElement.textContent = `${entry.name}: ${entry.score}`;
-    leaderboard.appendChild(entryElement);
-  });
-}
-
-// Initial display of the leaderboard
-displayLeaderboard();
-
-// Example: Adding a new entry
-addEntry('New Player', 95);
-displayLeaderboard();
